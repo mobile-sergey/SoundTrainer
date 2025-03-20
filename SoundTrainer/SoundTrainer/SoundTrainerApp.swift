4ebc76cb14e3a4c942f653189c32606f0527158b
Sergey Lavrov <lavrov-sergey@yandex.ru>
2025-03-20 17:20:59 +0300

    init project

---
+++ SoundTrainer/SoundTrainer/SoundTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundTrainerApp.swift
+//  SoundTrainer
+//
+//  Created by Sergey on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
